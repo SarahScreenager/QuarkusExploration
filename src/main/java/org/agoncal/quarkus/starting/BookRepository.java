93bd1ef437e1c47cb6c23c45feac96a455bb6b79
Sarah Hall <sarah.hall@live.co.uk>
2022-09-19 16:21:10 +0100

    Adding config options including profiles

--- src/main/java/org/agoncal/quarkus/starting/BookRepository.java
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -1,5 +1,7 @@
 package org.agoncal.quarkus.starting;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 import java.util.Optional;
@@ -7,12 +9,15 @@ import java.util.Optional;
 @ApplicationScoped
 public class BookRepository {
 
+    @ConfigProperty(name = "books.genre", defaultValue  = "Fantasy")
+    String genre;
+
     public List<Book> getAllBooks() {
         return List.of(
-                new Book(1, "BookOne", "Sarah", 2021, "N/A"),
-                new Book( 2, "Fellowship", "Tolkein", 1950, "Fantasy"),
-                new Book(3, "Two Towers", "Tolkein", 1950, "Fantasy"),
-                new Book(4, "Return of the King", "Tolkein", 1950, "Fantasy")
+                new Book(1, "BookOne", "Sarah", 2021, genre),
+                new Book( 2, "Fellowship", "Tolkein", 1950, genre),
+                new Book(3, "Two Towers", "Tolkein", 1950, genre),
+                new Book(4, "Return of the King", "Tolkein", 1950, genre)
         );
     }
 
